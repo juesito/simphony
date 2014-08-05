4dcd6716b4b01be139b58ffc76ed7aa68eea10ff
oj19edal <oj19edal@MXQRMNXL4021G8P.jaframx.com>
2014-08-04 19:28:32 -0500

    04082014 17:15 Tipos de pago II

--- src/main/java/com/simphony/entities/Customer.java
+++ src/main/java/com/simphony/entities/Customer.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
@@ -14,38 +13,9 @@ import javax.persistence.Table;
 @Table(name="Cliente")
 public  class Customer extends Person implements Serializable, Cloneable {
 
-    @Column(name="plataforma",length=25)
+    @Column(name="nombreCompleto",length=50)
     @Basic
-    private String platform;
-
-
-    @Column(name="estado",length=50)
-    @Basic
-    private String state;
-
-
-    @Column(name="telefono",length=10)
-    @Basic
-    private String phoneId;
-
-
-    @OneToOne(targetEntity=User.class)
-    private User user;
-
-
-    @Column(name="seccion",length=15)
-    @Basic
-    private String section;
-
-
-    @Column(name="NoFicha",length=10)
-    @Basic
-    private String keyId;
-
-
-    @Column(name="ciudad",length=50)
-    @Basic
-    private String city;
+    private String fullName;
 
     public Customer(){
 
@@ -53,13 +23,6 @@ public  class Customer extends Person implements Serializable, Cloneable {
 
     public void update(Associate associateUpdated){
         super.update(associateUpdated);
-        this.platform = associateUpdated.getPlatform();
-        this.keyId = associateUpdated.getKeyId();
-        this.city = associateUpdated.getCity();
-        this.phoneId = associateUpdated.getPhoneId();
-        this.section = associateUpdated.getSection();
-        this.state = associateUpdated.getState();
-        this.user = associateUpdated.getUser();
     }
     
     @PreUpdate
@@ -67,102 +30,6 @@ public  class Customer extends Person implements Serializable, Cloneable {
         super.setLastUpdate(new Date());
     }
 
-   public String getPlatform() {
-        return this.platform;
-    }
-
-
-  public void setPlatform (String platform) {
-        this.platform = platform;
-    }
-
-
-
-   public String getState() {
-        return this.state;
-    }
-
-
-  public void setState (String state) {
-        this.state = state;
-    }
-
-
-
-   public String getPhoneId() {
-        return this.phoneId;
-    }
-
-
-  public void setPhoneId (String phoneId) {
-        this.phoneId = phoneId;
-    }
-
-
-
-   public User getUser() {
-        return this.user;
-    }
-
-
-  public void setUser (User user) {
-        this.user = user;
-    }
-
-
-
-   public String getSection() {
-        return this.section;
-    }
-
-
-  public void setSection (String section) {
-        this.section = section;
-    }
-
-
-
-   public String getKeyId() {
-        return this.keyId;
-    }
-
-
-  public void setKeyId (String keyId) {
-        this.keyId = keyId;
-    }
-
-
-
-   public String getCity() {
-        return this.city;
-    }
-
-
-  public void setCity (String city) {
-        this.city = city;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 19 * hash + (this.platform != null ? this.platform.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Customer other = (Customer) obj;
-        if ((this.keyId == null) ? (other.keyId != null) : !this.keyId.equals(other.keyId)) {
-            return false;
-        }
-        return true;
-    }
     
     @Override
     public Object clone() throws CloneNotSupportedException {
---
+++ src/main/java/com/simphony/selectors/PayTypeBox.java
@@ -0,0 +1,61 @@
+package com.simphony.selectors;
+
+import com.simphony.beans.PayTypeService;
+import com.simphony.entities.PayType;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.model.SelectItem;
+
+/**
+ *
+ * @author root
+ */
+@ManagedBean(name = "boxPayTypeService", eager = true)
+@ApplicationScoped
+public class PayTypeBox {
+
+    @ManagedProperty(value = "#{payTypeService}")
+    private PayTypeService payTypeService;
+
+    private List<SelectItem> payTypeList = new ArrayList<SelectItem>();
+
+    public PayTypeBox() {
+    }
+    
+
+    @PostConstruct
+    public void init() {
+        List<PayType> optionList = new  ArrayList(); //this.getPayTypeService().getPayTypeRepository().findAllEnabled();
+
+        for (PayType payType : optionList) {
+            //payTypeList.add(new SelectItem(payType, payType.getDescription()));
+        }
+    }
+
+    public List<SelectItem> getPayTypeList() {
+        return payTypeList;
+    }
+
+    public void setPayTypeList(List<SelectItem> payTypeList) {
+        this.payTypeList = payTypeList;
+    }
+
+    public PayTypeService getPayTypeService() {
+        return payTypeService;
+    }
+
+    public void setPayTypeService(PayTypeService payTypeService) {
+        this.payTypeService = payTypeService;
+    }
+
+    public void fillBox() {
+        payTypeList.clear();
+        init();
+    }
+
+
+}
