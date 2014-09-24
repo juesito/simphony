b9f5d858b38594ac8073dbc80659d33a3a9e253d
Jueser <jues40@hotmail.com>
2014-09-24 17:08:15 -0500

    Impresoras
    Conflicts:
    	src/main/webapp/operation/guides/detailList.xhtml

--- src/main/java/com/simphony/managedbeans/SaleBean.java
+++ src/main/java/com/simphony/managedbeans/SaleBean.java
@@ -266,16 +266,25 @@ public class SaleBean implements IConfigurable {
 
                 if (seat.contains(reserved.getSeat())) {
                     int index = seat.indexOf(reserved.getSeat());
-                    //Parten del mismo origen   
-                    if (reserved.getFinalSequence() == 0) {
+                    
+                    if (this.selected.getItinerary().getSequence() == 0
+                            && this.selected.isNormalMode()) {
+                        seat.set(index, occupiedPattern);
+                    } else if (reserved.getFinalSequence() == 0) {  //Parten del mismo origen   
                         seat.set(index, occupiedPattern);
                     } else if (selected.getItinerary().getSequence() == reserved.getInitialSequence()) {
                         seat.set(index, occupiedPattern);
                         //Alt 10  -  Initial 0
-                    } else if (selected.getItinerary().getSequence() > reserved.getInitialSequence()
-                            && selected.getAlternateItinerary().getSequence() <= reserved.getFinalSequence()) {
+                    } else if (!this.selected.isNormalMode() && 
+                            (selected.getAlternateItinerary().getSequence() <= reserved.getFinalSequence() &&
+                            selected.getAlternateItinerary().getSequence() > reserved.getInitialSequence())) {
+                        seat.set(index, occupiedPattern);
+                    } else if (!this.selected.isNormalMode() && 
+                            (reserved.getInitialSequence() >= selected.getItinerary().getSequence() &&
+                            selected.getAlternateItinerary().getSequence() >= reserved.getFinalSequence())) {
                         seat.set(index, occupiedPattern);
                     }
+                    
                 }
             }
 
@@ -364,8 +373,8 @@ public class SaleBean implements IConfigurable {
             } else {
                 dtSale.setType(_SALE_TYPE_ASSOCIATE);
             }
-            
-            if(dtSale.getBolType().equals(_RETIREE)){
+
+            if (dtSale.getBolType().equals(_RETIREE)) {
                 dtSale.setDiscount(dtSale.getAmount() * _RETIREE_DISCOUNT);
             }
 
@@ -379,7 +388,11 @@ public class SaleBean implements IConfigurable {
 
             ///Aqui modifique si es local la ruta antes tenia 0 0
             reservedSeat.setInitialSequence(this.selected.getItinerary().getSequence());
-            reservedSeat.setFinalSequence(this.selected.getAlternateItinerary().getSequence());
+            if (this.selected.isNormalMode()) {
+                reservedSeat.setFinalSequence(this.selected.getItinerary().getSequence());
+            } else {
+                reservedSeat.setFinalSequence(this.selected.getAlternateItinerary().getSequence());
+            }
             reservedSeat.setSeat(dtSale.getSeat());
 
             saleService.getReservedSeatsRepository().save(reservedSeat);
@@ -784,7 +797,7 @@ public class SaleBean implements IConfigurable {
 
             }
             sale = saleDetailCancelled.getSale();
-            sale.setCancelUser(user.getNick());            
+            sale.setCancelUser(user.getNick());
             sale.setAmount(sale.getAmount() - saleDetailCancelled.getAmount());
             sale.setSubTotal(sale.getSubTotal() - (saleDetailCancelled.getAmount() - saleDetailCancelled.getDiscount()));
             sale.setPassengers(sale.getPassengers() - 1);
